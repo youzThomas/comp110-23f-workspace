cda7e7d6cead16fa96890ba92b9db597c0a40427
Zhuoran Thomas You <youz@unc.edu>
2023-09-11 21:38:32 -0400

    changes

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -13,26 +13,26 @@ if len(character) != 1:
     exit()
 
 count: int = 0
-print("Searching for "+character+" in "+word)
+print("Searching for " + character + " in " + word)
 
 if character == word[0]:
-    print(character+" found at index 0")
-    count+=1
+    print(character + " found at index 0")
+    count += 1
 
 if character == word[1]:
-    print(character+" found at index 1")
-    count+=1
+    print(character + " found at index 1")
+    count += 1
 
 if character == word[2]:
-    print(character+" found at index 2")
-    count+=1
+    print(character + " found at index 2")
+    count += 1
 
 if character == word[3]:
-    print(character+" found at index 3")
-    count+=1
+    print(character + " found at index 3")
+    count += 1
 
 if character == word[4]:
-    print(character+" found at index 4")
-    count+=1
+    print(character + " found at index 4")
+    count += 1
 
-print(str(count)+" instance of "+character+" found in "+word)
+print(str(count) + " instance of " + character + " found in " + word)
