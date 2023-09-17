ab840ce5f3ec260cb2d4438de63a17d73f1cfe4c
Zhuoran Thomas You <youz@unc.edu>
2023-09-17 18:51:31 -0400

    Attempt #2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,7 +5,7 @@ secret_word: str = "python"
 guess_word: str = input("What is your 6-letter guess?")
 
 while len(guess_word) != 6:
-    guess_word: str = input("That was not 6 letters! Try again:")
+    guess_word = input("That was not 6 letters! Try again:")
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -13,20 +13,20 @@ YELLOW_BOX: str = "\U0001F7E8"
 index_tracker: int = 0
 result: str = ""
 
-while index_tracker<6:
+while index_tracker < 6:
     if guess_word[index_tracker] == secret_word[index_tracker]:
         result = result + GREEN_BOX
     else:
         exist_tracker: bool = False
         alternate_index: int = 0
 
-        while exist_tracker == False and alternate_index<6:
+        while exist_tracker is False and alternate_index < 6:
             if guess_word[index_tracker] == secret_word[alternate_index]:
                 exist_tracker = True
             else:
-                alternate_index = alternate_index +1
+                alternate_index = alternate_index + 1
         
-        if exist_tracker == True:
+        if exist_tracker is True:
             result = result + YELLOW_BOX
         else:
             result = result + WHITE_BOX
@@ -37,6 +37,3 @@ if guess_word == secret_word:
     print(f"{result}\n Woo! You got it!")
 else:
     print(f"{result}\n Not quite. Play again soon!")
-
-
-
