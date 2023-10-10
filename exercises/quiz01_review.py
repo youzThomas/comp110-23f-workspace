8a41db8c8d4aa9fd474c51cd2653e8a8415ebd8d
Zhuoran Thomas You <youz@unc.edu>
2023-10-09 20:13:17 -0400

    change

---
+++ exercises/quiz01_review.py
@@ -0,0 +1,16 @@
+"""classification function"""
+a: int = 1
+print(type(a))
+
+"""valid bool"""
+b: bool = True
+c: bool = False
+
+"""int cannot start with a 0, but can end with a 0"""
+
+"""f-string output"""
+
+
+
+message: str = f"Hello {a}?"
+print(message)
