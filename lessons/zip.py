1cb2ba8d4e17677d071b3a36cadf8e95e6167fbc
Zhuoran Thomas You <youz@unc.edu>
2023-10-27 15:49:39 -0400

    zip

--- lessons/zip.py
+++ lessons/zip.py
@@ -6,10 +6,10 @@ def zip(string_list: list[str], integral_list: list[int]) -> dict[str, int]:
     """Transfer the values in two lists into one dictionary."""
     a: dict[str, int] = {}
 
-    if(len(string_list) == len(integral_list)):
+    if len(string_list) == len(integral_list):
         tracker: int = 0
 
-        while(tracker < len(string_list)):
+        while tracker < len(string_list):
             a.update({string_list[tracker]: integral_list[tracker]})
             tracker = tracker + 1
     
