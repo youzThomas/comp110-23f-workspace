6ba974dafd95852264fedef2b64da63504736291
Zhuoran Thomas You <youz@unc.edu>
2023-11-05 20:28:39 -0500

    CQ07_v2

--- lessons/CQ07/make_points.py
+++ lessons/CQ07/make_points.py
@@ -2,6 +2,6 @@
 __author__ = "730679279"
 from lessons.CQ07.point import Point
 
-new_point: Point(1.2, 1.4)
+new_point = Point(1.2, 1.4)
 new_point.scale_by(new_point, 2)
 new_point.scale(new_point, 3)
