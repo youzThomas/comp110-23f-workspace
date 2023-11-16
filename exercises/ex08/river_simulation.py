bae9e1a7d03ad06eb942307679a21e79b8605fcd
Zhuoran Thomas You <youz@unc.edu>
2023-11-15 22:40:38 -0500

    .

--- exercises/ex08/river_simulation.py
+++ exercises/ex08/river_simulation.py
@@ -1,3 +1,6 @@
 """Simulation."""
 from exercises.ex08.river import River
-river_test: River(10, 2)
+river_test = River(10, 2)
+river_test.view_river()
+river_test.one_river_day()
+river_test.view_river()
